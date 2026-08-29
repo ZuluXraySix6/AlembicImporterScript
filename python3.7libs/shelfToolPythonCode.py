f6c2203460fa820f0731234e2ee92ad025b11c10
ZuluXraySix6 <108427116+ZuluXraySix6@users.noreply.github.com>
2026-08-29 23:19:11 +0530

    Update shelfToolPythonCode.py

--- python3.7libs/shelfToolPythonCode.py
+++ python3.7libs/shelfToolPythonCode.py
@@ -6,17 +6,11 @@ import hou
 
 il.reload(ai)
 
-# Check whether the window already exists
-if hasattr(hou.session, "abcImporterWindow"):
-    try:
-        if hou.session.abcImporterWindow.isVisible():
-            hou.session.abcImporterWindow.raise_()
-            hou.session.abcImporterWindow.activateWindow()
-        else:
-            hou.session.abcImporterWindow.show()
-    except:
-        hou.session.abcImporterWindow = ai.MainWindow()
-        hou.session.abcImporterWindow.show()
-else:
-    hou.session.abcImporterWindow = ai.MainWindow()
-    hou.session.abcImporterWindow.show()
+try:
+    hou.session.abcImporterWindow.close()
+    hou.session.abcImporterWindow.deleteLater()
+except:
+    pass
+
+hou.session.abcImporterWindow = ai.MainWindow()
+hou.session.abcImporterWindow.show()
